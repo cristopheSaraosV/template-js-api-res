65eddd0e74ccdb7904ffde7058e99dfc57fc4bee
cristopheSaraosV <cristopher.saraos@gmail.com>
2021-05-16 22:18:51 -0500

    first version api

--- models/server.js
+++ models/server.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-const routerUsers = require('../routes/users.routes')
+const routerUsers = require('../routes/users.routes');
+const { dbConnection } = require('../database/config');
+
 class Server {
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT;
+
+		// DB
+		this.connectDB();
+
 		// Path
-		this.usersPath = '/api/users'
+		this.usersPath = '/api/users';
 		// Middleware
 		this.middlewares();
 		// Routes
 		this.routes();
 	}
 
+	async connectDB (){
+		await dbConnection();
+	}
+
 	middlewares() {
-		this.app.use( cors() );
+		this.app.use(cors());
 		this.app.use(express.static('public'));
-		this.app.use( express.json() );
+		this.app.use(express.json());
 	}
 
 	// Routes
 	routes() {
-		this.app.use(this.usersPath,routerUsers)
+		this.app.use(this.usersPath, routerUsers);
 	}
 
 	listen() {
